ddfbd0cdb714013fd7527b4ab39bc6811311a7a2
Renato Balcazar <renatojbl99@gmail.com>
2019-08-01 22:07:35 -0500

    Creando clase de encriptado

---
+++ app/src/main/java/com/renatojbl99/keykeeper/Encrypted/Encrypt.java
@@ -0,0 +1,25 @@
+package com.renatojbl99.keykeeper.Encrypted;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encrypt {
+
+    public String getEncrypt(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
